428b6edbd87ac7ead248ec705a6c4782cd04d76b
magnus.prime <78599959+morikeli@users.noreply.github.com>
2025-11-13 18:06:08 +0300

    feat: created auth schemas

---
+++ app/schemas/auth_schema.py
@@ -0,0 +1,28 @@
+from pydantic import BaseModel, EmailStr
+
+
+class LoginResponse(BaseModel):
+    """This is a login response model. It returns json data with the fields below."""
+
+    access_token: str
+    token_type: str
+
+
+class LoginRequest(BaseModel):
+    """This is a login request model. It accepts response data with the fields below."""
+
+    username: str
+    password: str
+
+
+class RequestEmailVerificationSchema(BaseModel):
+    email: EmailStr
+
+
+class ResetPasswordSchema(BaseModel):
+    email: EmailStr
+
+
+class ConfirmResetPasswordSchema(BaseModel):
+    new_password: str
+    confirm_new_password: str
