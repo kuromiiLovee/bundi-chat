d1f9bdf2bb4ceeab571472694d69dadb21483539
magnus.prime <78599959+morikeli@users.noreply.github.com>
2025-11-14 09:31:03 +0300

    feat: imported new schemas

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -5,18 +5,29 @@ from .auth_schema import (
     RequestEmailVerificationSchema,
     ResetPasswordSchema,
 )
-from .room_schema import CreateRoom, RoomResponse
+from .room_schema import (
+    ChatRoom,
+    CreateRoom,
+    JoinChatRoom,
+    JoinChatRoomResponse,
+    RoomMembers,
+    RoomResponse,
+)
 from .user_schema import CreatedUserResponse, CreateUser, User, UserResponse
 
 __all__ = [
     "ConfirmResetPasswordSchema",
+    "ChatRoom",
     "CreateRoom",
     "CreateUser",
     "CreatedUserResponse",
+    "JoinChatRoom",
+    "JoinChatRoomResponse",
     "LoginRequest",
     "LoginResponse",
     "RequestEmailVerificationSchema",
     "ResetPasswordSchema",
+    "RoomMembers",
     "RoomResponse",
     "User",
     "UserResponse",
