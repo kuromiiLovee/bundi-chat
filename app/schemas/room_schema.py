351fdbe0fabd80928a3639f8bb46af65d50ff80d
magnus.prime <78599959+morikeli@users.noreply.github.com>
2025-11-14 09:38:32 +0300

    fix: re-organized code in script and created new schemas

--- app/schemas/room_schema.py
+++ app/schemas/room_schema.py
@@ -22,16 +22,7 @@ class CreateRoom(ChatRoom):
     pass
 
 
-class RoomMembers(BaseModel):
-    """
-    This schema represents response data for all group members.
-    """
-
-    member: str
-    room: str
-
-
-class RoomResponse(Room):
+class RoomResponse(ChatRoom):
     """
     This schema that represents a response data for a chat room.
     """
@@ -40,8 +31,51 @@ class RoomResponse(Room):
     host: User
     link: str
     password: str
-    members: Optional[List[RoomMembers]] = []
     created_at: datetime
+
+    class Config:
+        from_attributes = True
+
+
+class RoomMember(BaseModel):
+    """
+    This schema represent data for a single member in a chat room.
+    """
+
+    member: User
+
+    class Config:
+        from_attributes = True
+
+
+class RoomMembers(BaseModel):
+    """
+    This schema represents response data for all members in a chat room.
+    """
+
+    member: str
+    room: str
+
+
+
+class JoinChatRoom(BaseModel):
+    """
+    Serialize request body when a user wants to join a room.
+    """
+
+    password: str
+
+    class Config:
+        from_attributes = True
+
+
+class JoinChatRoomResponse(BaseModel):
+    """
+    This schema represents response data when a user joins a chat room.
+    """
+
+    room: RoomResponse
+    members: List[RoomMember]
 
     class Config:
         from_attributes = True
